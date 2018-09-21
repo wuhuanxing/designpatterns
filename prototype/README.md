4020e6c23fc410118e35f897031a6174f6764aa3
huanxing.wu <huanxing.wu@mobilemd.cn>
2018-09-21 09:26:47 +0800

    添加prototype原型模式

---
+++ prototype/README.md
@@ -0,0 +1,3 @@
+Prototype design pattern is used when the Object creation is a costly affair and requires a lot of time and resources and you have a similar object already existing.
+
+Prototype pattern provides a mechanism to copy the original object to a new object and then modify it according to our needs. Prototype design pattern uses java cloning to copy the object.
---
+++ prototype/src/main/java/com/huanxing/pattern/prototype/Employees.java
@@ -0,0 +1,42 @@
+package com.huanxing.pattern.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ *@author huanxing.wu at 2018/09/21
+ */
+public class Employees implements Cloneable {
+
+	private List<String> empList;
+
+	public Employees() {
+		empList = new ArrayList<String>();
+	}
+
+	public Employees(List<String> list) {
+		this.empList = list;
+	}
+
+	public void loadData() {
+		//read all employees from database and put into the list
+		empList.add("Pankaj");
+		empList.add("Raj");
+		empList.add("David");
+		empList.add("Lisa");
+	}
+
+	public List<String> getEmpList() {
+		return empList;
+	}
+
+	@Override public Object clone() throws CloneNotSupportedException {
+		List<String> temp = new ArrayList<String>();
+		for (String s : this.getEmpList()) {
+			temp.add(s);
+		}
+		return new Employees(temp);
+	}
+
+}
